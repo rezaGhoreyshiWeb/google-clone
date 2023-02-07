ebd8a48ac7dea4862427ac5c6b6bd6d931987a40
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2023-02-07 10:26:27 +0330

    add Home page file

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages";
 
-const router = createBrowserRouter([])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
